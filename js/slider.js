b3c5128cb65b426d290e1a3729bb8d15d71db3a7
Алексей Андрияненко <lex_flame@mail.ua>
2018-12-16 11:46:12 +0200

    Сделал и адаптировал слайдер

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.projects-slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    nextArrow: '<div class="arrow-right arrow"></div>',
+    prevArrow: '<div class="arrow-left arrow"></div>',
+    responsive: [
+      {
+        breakpoint: 998,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      }
+    ]
+});
+});
