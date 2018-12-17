f13aa495995c9786ade0d4fcfc9570355e39fef3
Алексей Андрияненко <lex_flame@mail.ua>
2018-12-18 00:31:31 +0200

    Осталось сделать модальное окно

--- js/slider.js
+++ js/slider.js
@@ -6,11 +6,28 @@ $(document).ready(function(){
     prevArrow: '<div class="arrow-left arrow"></div>',
     responsive: [
       {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          nextArrow: '<div class="arro0w-right arro0w"></div>',
+          prevArrow: '<div class="arro0w-left arro0w"></div>',
+        }
+      },
+      {
         breakpoint: 998,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          infinite: true,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          nextArrow: '<div class="arro00w-right arro00w"></div>',
+          prevArrow: '<div class="arro00w-left arro00w"></div>',
         }
       }
     ]
